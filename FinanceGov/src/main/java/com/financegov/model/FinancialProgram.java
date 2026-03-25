d1ede848eaf9b6bf495a09dff5d2490f0746681e
aesha <aesharoy26@gmail.com>
2026-03-25 11:32:20 +0530

    financial program model and enum

--- FinanceGov/src/main/java/com/financegov/model/FinancialProgram.java
+++ FinanceGov/src/main/java/com/financegov/model/FinancialProgram.java
@@ -1,54 +1,39 @@
 package com.financegov.model;
 
 import java.time.LocalDate;
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import com.financegov.enums.ProgramStatus;
 
 @Entity
 @Table(name = "financial_program")
-@Data                     // Generates getters, setters, toString, equals, hashCode
-@NoArgsConstructor        
-@AllArgsConstructor       
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class FinancialProgram {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long programId;
 
-    @NotBlank(message = "Title cannot be blank")
+    @NotNull
     private String title;
 
-    @NotBlank(message = "Description cannot be blank")
     private String description;
 
-    @NotNull(message = "Start date is required")
-    private LocalDate startDate;
-
-    @NotNull(message = "End date is required")
-    @Future(message = "End date must be in the future")
-    private LocalDate endDate;
-
-    @NotNull(message = "Budget is required")
-    @Positive(message = "Budget must be a positive value")
+    @Min(value = 0, message = "Budget must be non-negative")
     private Double budget;
 
-    @NotBlank(message = "Status cannot be blank")
-    private String status;
-    
-    @OneToMany(mappedBy = "program", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<SubsidyApplication> applications;
+    @NotNull
+    private LocalDate startDate;
+
+    @NotNull
+    private LocalDate endDate;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private ProgramStatus status;
 }
